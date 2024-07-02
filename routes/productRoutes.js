d38c4a45226af120a172df1c6ab94c54873de77e
Ahmed Baig <ahmedbaig137@gmail.com>
2024-07-03 04:18:28 +0500

    Updated products

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -12,10 +12,10 @@ const router = express.Router();
 router.post('/products', validator.validateProduct, auth.AuthRole("SuperAdmin"), auth.AuthRole("Admin"), productController.createProduct);
 
 // router.get('/products/:id', productController.getProduct);
-router.get('/products/:id', validator.validateProductId, productController.getProduct);
+router.get('/products/:id', validator.validateProductId, auth.AuthRole("SuperAdmin"), auth.AuthRole("Admin"), productController.getProduct);
 
-router.put('/products/:id', productController.updateProduct, auth.AuthRole("SuperAdmin"), auth.AuthRole("Admin"));
-router.delete('/products/:id', productController.deleteProduct, auth.AuthRole("SuperAdmin"), auth.AuthRole("Admin"));
-router.get('/products', productController.getAllProducts, auth.AuthRole("SuperAdmin"), auth.AuthRole("Admin"));
+router.put('/products/:id', auth.AuthRole("SuperAdmin"), auth.AuthRole("Admin"), productController.updateProduct );
+router.delete('/products/:id', auth.AuthRole("SuperAdmin"), auth.AuthRole("Admin"), productController.deleteProduct);
+router.get('/products', auth.AuthRole("SuperAdmin"), auth.AuthRole("Admin"), productController.getAllProducts);
 
 module.exports = router;
